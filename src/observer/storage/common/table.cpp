ab61fa748293ef1d3e5cb799d510c9600db9e9cc
yumengmccc <14781163356@163.com>
2025-06-16 01:33:58 +0800

    'init'

--- src/observer/storage/common/table.cpp
+++ src/observer/storage/common/table.cpp
@@ -943,12 +943,6 @@ RC Table::destroy(const char* dir) {
         return RC::GENERIC_ERROR;
     }
 
-    std::string text_data_file = std::string(dir) + "/" + name() + TABLE_TEXT_DATA_SUFFIX;
-    if(unlink(text_data_file.c_str()) != 0) { // 删除表实现text字段的数据文件（后续实现了text case时需要考虑，最开始可以不考虑这个逻辑）
-        LOG_ERROR("Failed to remove text data file=%s, errno=%d", text_data_file.c_str(), errno);
-        return RC::GENERIC_ERROR;
-    }
-
     const int index_num = table_meta_.index_num();
     for (int i = 0; i < index_num; i++) {  // 清理所有的索引相关文件数据与索引元数据
         ((BplusTreeIndex*)indexes_[i])->close();
